1e1366e1bf47c55784ee023fcd89b15378a94902
Nguyen Ngoc Duy <hotboyarsenal@gmail.com>
2020-06-19 17:10:08 +0700

    update

--- demo/index.php
+++ demo/index.php
@@ -13,8 +13,8 @@ if (isset($_GET['controller']))
         $action = 'index';
     }
 } else {
-    $controller = 'pages';
-    $action = 'home';
+    $controller = 'posts';
+    $action = 'index';
 }
 require_once('routes.php');
 ?>
